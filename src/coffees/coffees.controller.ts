54291a480d8e174d147963c7513d266847e88990
wangchulin <13595451343@163.com>
2023-07-31 22:17:11 +0800

    feat: services simply CRUD

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -4,42 +4,39 @@ import { CoffeesService } from './coffees.service';
 @Controller('coffees')
 //* controller使用构造函数、ts类型声明连接service，并创建实例
 export class CoffeesController {
-    constructor(private readonly coffeesServices:CoffeesService){
-
-    }
+    constructor(private readonly coffeesService:CoffeesService){ }
 
 
     // * Get装饰器，客户端对coffees路由发get请求时，进行处理的函数
     // * Query参数对数据进行过滤、分组、排序等操作
     @Get()
     findAll(@Query() paginationQuery:any){
-        const{limit,offset}=paginationQuery;
-        return 'this is all sorts of coffees'
+        // const{limit,offset}=paginationQuery;
+        return this.coffeesService.findAll()
     }
 
     // * 动态路由，获取param中的id
     @Get(':id')
     findOne(@Param('id') id:string){
-        return `this is 
-        ${id} coffee`;
+       return this.coffeesService.findOne(id)
     }
 
     //* POST获取请求体内容
     // ! HttpCode修改状态码（处理错误）
     @Post()
     create(@Body('name') body:object){
-        return body
+        return this.coffeesService.create(body)
     }
     //* Patch和Delete方法
     @Patch(':id')
     update(@Param('id') id:string,@Body() body:any){
-        return `update ${id} coffees`
+        return this.coffeesService.update(id,body)
         
     }
 
     @Delete('id')
     remove(@Param('id') id:string){
-        return `delete ${id} coffees`
+        return this.coffeesService.remove(id)
 
     }
 
--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -5,12 +5,44 @@ import { Coffee } from './entities/coffee.entity';
 export class CoffeesService {
     //模拟数据
     //TODOS:连接真实数据库
-    private coffees:Coffee=
-        {
+    private coffees:Coffee[]=
+       [ {
             id:1,
             name:'orangeCoffee',
             brand:'lucky',
             flavors:['orange','coffee']
-        }
+        },
+        {
+            id:2,
+            name:'blackCoffee',
+            brand:'costa',
+            flavors:['coffee']
+        },
+
+    ];
+    findAll(){
+        return this.coffees;
+    }
     
+    findOne(id:string){
+        return this.coffees.find(item=>item.id=== +id)
+    }
+
+    create(createInstance:any){
+        this.coffees.push(createInstance)
+    }
+
+    update(id:string,updateInstance:any){
+        const existCoffee=this.findOne(id);
+        if(existCoffee){
+            // TODO 
+        }
+    }
+
+    remove(id:string){
+        const coffeeIndex=this.coffees.findIndex(item=>item.id===+id);
+        if(coffeeIndex >=0){
+            this.coffees.splice(coffeeIndex,1);
+        }
+    }
 }
