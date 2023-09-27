1f2a7a86be1867f77d02d31833ac9a47f188d747
wangchulin <13595451343@163.com>
2023-09-27 10:06:01 +0800

    chore: update .gitignore

--- src/common/constant/alicloud.ts
+++
@@ -1,3 +0,0 @@
-export const ACCESS_KEY='LTAI5tBrr3VyztxV4mPZAzwi';
-export const ACCESS_KEY_SECRET='4uEcCAFMbBxoPXq2oKRqL20v08CQP3';
-export const OSS_URL='https://yikina-pic-app.oss-cn-beijing.aliyuncs.com/';
